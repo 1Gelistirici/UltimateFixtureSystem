3966e320bffb4ac1874cc86457c32de9e73cd072
Onur <onurylmz00@gmail.com>
2021-10-14 00:54:31 +0300

    Reports için api güncellemesi

---
+++ UltimateAPI/Controllers/ReportController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltimateAPI.CallManager;
+using UltimateAPI.Entities;
+
+namespace UltimateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpPost("AddReport")]
+        public IActionResult AddReport(Report parameter)
+        {
+            var result = ReportCallManager.Instance.AddReport(parameter);
+            return Content(ResultData.Get(result.IsSuccess, result.Message, result.Data));
+        }
+
+        [HttpGet("GetReports")]
+        public IActionResult GetReports()
+        {
+            var result = ReportCallManager.Instance.GetReports();
+            return Content(ResultData.Get(result.IsSuccess, result.Message, result.Data));
+        }
+    }
+}
