aebb20a7ad04c161b7a27358d7b35622f4be0d70
İsmail Gürsel <ismail@asisotomasyon.com>
2021-11-01 10:40:43 +0300

    changed

--- UltimateAPI/Manager/ReportManager.cs
+++ UltimateAPI/Manager/ReportManager.cs
@@ -37,7 +37,7 @@ namespace UltimateAPI.Manager
 
             try
             {
-                using (sqlConnection = new SqlConnection(@"Data Source=SKY-NET\SQLEXPRESS;Initial Catalog=UDemirbas;Integrated Security=True"))
+                using (sqlConnection = Global.GetSqlConnection())
                 {
                     ConnectionManager.Instance.SqlConnect(sqlConnection);
 
@@ -88,7 +88,7 @@ namespace UltimateAPI.Manager
 
             try
             {
-                using (sqlConnection = new SqlConnection(@"Data Source=SKY-NET\SQLEXPRESS;Initial Catalog=UDemirbas;Integrated Security=True"))
+                using (sqlConnection = Global.GetSqlConnection())
                 {
                     ConnectionManager.Instance.SqlConnect(sqlConnection);
 
