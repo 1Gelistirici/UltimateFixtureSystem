955c8e6af0f6ddf6152c614fae59025e4abe4927
İsmail Gürsel <ismail@asisotomasyon.com>
2021-11-02 11:50:52 +0300

    Db adresi iki pc arasında oto değişecek

--- UltimateAPI/Tool/Global.cs
+++ UltimateAPI/Tool/Global.cs
@@ -6,8 +6,13 @@ namespace UltimateAPI.Manager
     {
         public static SqlConnection GetSqlConnection()
         {
+            string MachineName = System.Net.Dns.GetHostName();
+            if (MachineName == "yaz31")
+            {
+                return new SqlConnection(@"Data Source=YAZ31;Initial Catalog=UDemirbas;Integrated Security=True");
+            }
+
             return new SqlConnection(@"Data Source=SKY-NET\SQLEXPRESS;Initial Catalog=UDemirbas;Integrated Security=True");
-            //@"Data Source=SKY-NET\SQLEXPRESS;Initial Catalog=UDemirbas;Integrated Security=True"
         }
     }
 }
