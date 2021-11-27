43b4dc0d8ad3f90eab225aa45ef405fdd9bde53b
Onur <onurylmz00@gmail.com>
2021-11-27 03:15:51 +0300

    Changed

---
+++ UltimateAPI/Entities/Fixture.cs
@@ -0,0 +1,13 @@
+﻿
+namespace UltimateAPI.Entities
+{
+    public class Fixture : BaseProperty
+    {
+        public int LoginSystem { get; set; }
+        public int ModelNo { get; set; }
+        public int BillNo { get; set; }
+        public int StatuNo { get; set; }
+        public int CategoryNo { get; set; }
+        public int userNo { get; set; }
+    }
+}
--- UltimateDemerbas/wwwroot/AngularControllers/Controllers/BillController.js
+++ UltimateDemerbas/wwwroot/AngularControllers/Controllers/BillController.js
@@ -1,125 +1,126 @@
-﻿MainApp.controller("BillController", ["$scope", "BillService", "BillTypeService", "$http", "NgTableParams", function ($scope, BillService, BillTypeService, $http, NgTableParams) {
+﻿MainApp.controller("BillController", ["$scope", "BillService", "BillTypeService", "$http", "NgTableParams", "toaster",
+    function ($scope, BillService, BillTypeService, $http, NgTableParams, toaster) {
 
-    $scope.RegisterCount = 0;
-    $scope.Pop = [];
+        $scope.RegisterCount = 0;
+        $scope.Pop = [];
 
-    $scope.TableCol = {
-        BillNo: "Bill No",
-        BillDate: "Bill Date",
-        Price: "Price",
-        Comment: "Comment",
-        Product: "Product",
-        Piece: "Piece",
-        TypeNo: "Bill Type",
-        Items: "Items",
-    };
+        $scope.TableCol = {
+            BillNo: "Bill No",
+            BillDate: "Bill Date",
+            Price: "Price",
+            Comment: "Comment",
+            Product: "Product",
+            Piece: "Piece",
+            TypeNo: "Bill Type",
+            Items: "Items",
+        };
 
-    $scope.ttt = function () {
-        return 1;
-    }
-
-    $scope.GetBillTypes = function () {
-        BillTypeService.GetBillTypes(
-            function success(result) {
-                if (result.IsSuccess) {
-                    $scope.BillTypes = result.Data;
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-    $scope.GetBillTypes();
-
-    $scope.GetBills = function () {
-        BillService.GetBills(
-            function success(result) {
-                if (result.IsSuccess) {
-                    $scope.Data = result.Data;
-                    $scope.RegisterCount = $scope.Data.length;
-                    $scope.TableParams = new NgTableParams({
-                        sorting: { name: 'adc' },
-                        count: 20
-                    }, {
-                        counts: [10, 20, 50],
-                        dataset: $scope.Data
-                    });
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-    $scope.GetBills();
-
-    $scope.DeleteBill = function (data) {
-        BillService.DeleteBill(data.Id,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.UpdateBill = function (data) {
-        BillService.UpdateBill(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.AddBill = function () {
-
-        var data = {
-            "Name": $scope.Pop.Name,
-            "Piece": $scope.Pop.Piece,
-            "Boundary": $scope.Pop.Boundary,
-            "Price": $scope.Pop.Price
+        $scope.ttt = function () {
+            return 1;
         }
 
-        BillService.AddBill(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
+        $scope.GetBillTypes = function () {
+            BillTypeService.GetBillTypes(
+                function success(result) {
+                    if (result.IsSuccess) {
+                        $scope.BillTypes = result.Data;
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
                     toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
+                });
+        }
+        $scope.GetBillTypes();
 
-    }
+        $scope.GetBills = function () {
+            BillService.GetBills(
+                function success(result) {
+                    if (result.IsSuccess) {
+                        $scope.Data = result.Data;
+                        $scope.RegisterCount = $scope.Data.length;
+                        $scope.TableParams = new NgTableParams({
+                            sorting: { name: 'adc' },
+                            count: 20
+                        }, {
+                            counts: [10, 20, 50],
+                            dataset: $scope.Data
+                        });
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                });
+        }
+        $scope.GetBills();
+
+        $scope.DeleteBill = function (data) {
+            BillService.DeleteBill(data.Id,
+                function success(result) {
+                    if (result.IsSuccess) {
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                });
+        }
+
+        $scope.UpdateBill = function (data) {
+            BillService.UpdateBill(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Toner başarıyla güncellendi.");
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                });
+        }
+
+        $scope.AddBill = function () {
+            var data = {
+                "Name": $scope.Pop.Name,
+                "Piece": $scope.Pop.Piece,
+                "Boundary": $scope.Pop.Boundary,
+                "Price": $scope.Pop.Price
+            }
+
+            BillService.AddBill(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Toner başarıyla kaydedildi.");
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                });
+        }
 
 
-    //$(document).ready(function () {
-    //    var table = $('#example').DataTable();
+        //$(document).ready(function () {
+        //    var table = $('#example').DataTable();
 
-    //    $('#example tbody').on('click', 'tr', function () {
-    //        var data = table.row(this).data();
-    //        alert('You clicked on ' + data[0] + '\'s row');
-    //    });
-    //});
+        //    $('#example tbody').on('click', 'tr', function () {
+        //        var data = table.row(this).data();
+        //        alert('You clicked on ' + data[0] + '\'s row');
+        //    });
+        //});
 
-    //$scope.PopupForm = function () {
-    //    if (IsOpen) {
-    //        document.getElementById("myForm").style.display = "block";
-    //        IsOpen = false;
-    //    } else {
-    //        document.getElementById("myForm").style.display = "none";
-    //        IsOpen = true;
-    //    }
-    //}
+        //$scope.PopupForm = function () {
+        //    if (IsOpen) {
+        //        document.getElementById("myForm").style.display = "block";
+        //        IsOpen = false;
+        //    } else {
+        //        document.getElementById("myForm").style.display = "none";
+        //        IsOpen = true;
+        //    }
+        //}
 
-}]);
+    }]);
 
 
 
