e1a02dd8849f46fdf4aba7cd26bebab3dd17eff6
Onur <onurylmz00@gmail.com>
2021-12-05 04:51:39 +0300

    Changed

--- UltimateDemerbas/wwwroot/AngularControllers/Controllers/ComponentModelController.js
+++ UltimateDemerbas/wwwroot/AngularControllers/Controllers/ComponentModelController.js
@@ -1,73 +1,78 @@
-﻿MainApp.controller("ComponentModelController", ["$scope", "ComponentModelService", "$http", "NgTableParams", function ($scope, ComponentModelService, $http, NgTableParams) {
-    $scope.RegisterCount = 0;
-    $scope.Pop = [];
+﻿MainApp.controller("ComponentModelController", ["$scope", "ComponentModelService", "toaster", "NgTableParams",
+    function ($scope, ComponentModelService, toaster, NgTableParams) {
+        $scope.RegisterCount = 0;
+        $scope.Pop = [];
 
-    $scope.TableCol = {
-        Name: "Component Model Name",
-    };
+        $scope.TableCol = {
+            Name: "Component Model Name",
+        };
 
-    $scope.GetComponentModels = function () {
-        ComponentModelService.GetComponentModels(
-            function success(result) {
-                if (result.IsSuccess) {
-                    $scope.Data = result.Data;
-                    $scope.RegisterCount = $scope.Data.length;
-                    $scope.TableParams = new NgTableParams({
-                        sorting: { name: 'adc' },
-                        count: 20
-                    }, {
-                        counts: [10, 20, 50],
-                        dataset: $scope.Data
-                    });
-                } else {
+        $scope.GetComponentModels = function () {
+            ComponentModelService.GetComponentModels(
+                function success(result) {
+                    if (result.IsSuccess) {
+                        $scope.Data = result.Data;
+                        $scope.RegisterCount = $scope.Data.length;
+                        $scope.TableParams = new NgTableParams({
+                            sorting: { name: 'adc' },
+                            count: 20
+                        }, {
+                            counts: [10, 20, 50],
+                            dataset: $scope.Data
+                        });
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
                     toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-    $scope.GetComponentModels();
+                });
+        }
+        $scope.GetComponentModels();
 
-    $scope.DeleteComponentModel = function (data) {
-        ComponentModelService.DeleteComponentModel(data.Id,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.UpdateComponentModel = function (data) {
-        ComponentModelService.UpdateComponentModel(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.AddComponentModel = function () {
-
-        var data = {
-            "Name": $scope.Pop.Name,
+        $scope.DeleteComponentModel = function (data) {
+            ComponentModelService.DeleteComponentModel(data.Id,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Component model silme işlemi başarılı");
+                        $scope.GetComponentModels();
+                    } else {
+                        toaster.success("Başarısız", "Component model silme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Component model silme işlemi yapılırken bir hata oluştu");
+                });
         }
 
-        ComponentModelService.AddComponentModel(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
+        $scope.UpdateComponentModel = function (data) {
+            ComponentModelService.UpdateComponentModel(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Component model güncelleme işlemi başarılı");
+                    } else {
+                        toaster.success("Başarısız", "Component model güncelleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Component model güncelleme işlemi yapılırken bir hata oluştu");
+                });
+        }
 
-    }
+        $scope.AddComponentModel = function () {
+            var data = {
+                "Name": $scope.Pop.Name,
+            }
 
-}]);
+            ComponentModelService.AddComponentModel(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Component model ekleme işlemi başarılı");
+                        $('#AddSituation').modal('hide');
+                        $scope.GetComponentModels();
+                    } else {
+                        toaster.success("Başarısız", "Component model ekleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Component model ekleme işlemi yapılırken bir hata oluştu");
+                });
+        }
+
+    }]);
