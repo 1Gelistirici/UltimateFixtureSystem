d5c320ef4ae275d7221c8259b3d743f130c5cf0f
Onur <onurylmz00@gmail.com>
2021-12-05 05:12:14 +0300

    Changed

--- UltimateDemerbas/wwwroot/AngularControllers/Controllers/BillTypeController.js
+++ UltimateDemerbas/wwwroot/AngularControllers/Controllers/BillTypeController.js
@@ -1,73 +1,78 @@
-﻿MainApp.controller("BillTypeController", ["$scope", "BillTypeService", "$http", "NgTableParams", function ($scope, BillTypeService, $http, NgTableParams) {
-    $scope.RegisterCount = 0;
-    $scope.Pop = [];
+﻿MainApp.controller("BillTypeController", ["$scope", "BillTypeService", "toaster", "NgTableParams",
+    function ($scope, BillTypeService, toaster, NgTableParams) {
+        $scope.RegisterCount = 0;
+        $scope.Pop = [];
 
-    $scope.TableCol = {
-        Name: "Component Model Name",
-    };
+        $scope.TableCol = {
+            Name: "Component Model Name",
+        };
 
-    $scope.GetBillTypes = function () {
-        BillTypeService.GetBillTypes(
-            function success(result) {
-                if (result.IsSuccess) {
-                    $scope.Data = result.Data;
-                    $scope.RegisterCount = $scope.Data.length;
-                    $scope.TableParams = new NgTableParams({
-                        sorting: { name: 'adc' },
-                        count: 20
-                    }, {
-                        counts: [10, 20, 50],
-                        dataset: $scope.Data
-                    });
-                } else {
+        $scope.GetBillTypes = function () {
+            BillTypeService.GetBillTypes(
+                function success(result) {
+                    if (result.IsSuccess) {
+                        $scope.Data = result.Data;
+                        $scope.RegisterCount = $scope.Data.length;
+                        $scope.TableParams = new NgTableParams({
+                            sorting: { name: 'adc' },
+                            count: 20
+                        }, {
+                            counts: [10, 20, 50],
+                            dataset: $scope.Data
+                        });
+                    } else {
+                        toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
                     toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-    $scope.GetBillTypes();
+                });
+        }
+        $scope.GetBillTypes();
 
-    $scope.DeleteBillType = function (data) {
-        BillTypeService.DeleteBillType(data.Id,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.UpdateBillType = function (data) {
-        BillTypeService.UpdateBillType(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
-    }
-
-    $scope.AddBillType = function () {
-
-        var data = {
-            "Name": $scope.Pop.Name,
+        $scope.DeleteBillType = function (data) {
+            BillTypeService.DeleteBillType(data.Id,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Bill type silme işlemi yapılırken bir hata oluştu");
+                        $scope.GetBillTypes();
+                    } else {
+                        toaster.success("Başarısız", "Bill type silme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Bill type silme işlemi yapılırken bir hata oluştu");
+                });
         }
 
-        BillTypeService.AddBillType(data,
-            function success(result) {
-                if (result.IsSuccess) {
-                } else {
-                    toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-                }
-            }, function error() {
-                toaster.error("Kat listeleme", "Kat listeleme işlemi yapılırken bir hata oluştu");
-            });
+        $scope.UpdateBillType = function (data) {
+            BillTypeService.UpdateBillType(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Bill type güncelleme işlemi yapılırken bir hata oluştu");
+                    } else {
+                        toaster.success("Başarısız", "Bill type güncelleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Bill type güncelleme işlemi yapılırken bir hata oluştu");
+                });
+        }
 
-    }
+        $scope.AddBillType = function () {
+            var data = {
+                "Name": $scope.Pop.Name,
+            }
 
-}]);
+            BillTypeService.AddBillType(data,
+                function success(result) {
+                    if (result.IsSuccess) {
+                        toaster.success("Başarılı", "Bill type ekleme işlemi yapılırken bir hata oluştu");
+                        $('#AddSituation').modal('hide');
+                        $scope.GetBillTypes();
+                    } else {
+                        toaster.success("Başarısız", "Bill type ekleme işlemi yapılırken bir hata oluştu");
+                    }
+                }, function error() {
+                    toaster.success("Başarısız", "Bill type ekleme işlemi yapılırken bir hata oluştu");
+                });
+        }
+
+    }]);
