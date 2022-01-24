86dd84667f78949d0627a529875cdd7242e78aaf
İsmail Gürsel <ismail@asisotomasyon.com>
2022-01-24 13:15:32 +0300

    changed

--- UltimateAPI/Entities/Enums/LogType.cs
+++ UltimateAPI/Entities/Enums/LogType.cs
@@ -4,6 +4,10 @@ namespace UltimateAPI.Entities.Enums
     public enum LogType
     {
         LoginFailed = 0,
-        LoginSucess = 1
+        LoginSucess = 1,
+        Add = 2,
+        Delete = 3,
+        Update = 4,
+        Error = 5
     }
 }
