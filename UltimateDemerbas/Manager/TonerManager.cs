4f1e5a10c1a4c5ea541876c3d52a9b2e9dc3d322
İsmail Gürsel <ismail@asisotomasyon.com>
2022-01-28 15:26:19 +0300

    Changed

--- UltimateDemerbas/Manager/TonerManager.cs
+++ UltimateDemerbas/Manager/TonerManager.cs
@@ -32,43 +32,5 @@ namespace UltimateDemerbas.Manager
         {
             return GetApiParameter<Toner>("Toner/AddToner", parameter);
         }
-
-
-
-
-
-        //public async Task<string> GetToner(Login login)
-        //{
-        //    var url = $"https://localhost:44354/api/Toner/GetToner";
-        //    var httpClient = _httpClientFactory.CreateClient("Test"); ;
-        //    var JsonData = GetSerilizatiob<Login>(login);
-
-        //    try
-        //    {
-        //        var response = await httpClient.PostAsync(url, JsonData);
-        //        return await response.Content.ReadAsStringAsync();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return null;
-        //    }
-        //}
-
-        //public async Task<string> GetToners()
-        //{
-        //    var url = $"https://localhost:44354/api/Toner/GetToners";
-        //    var httpClient = _httpClientFactory.CreateClient("Test"); ;
-
-        //    try
-        //    {
-        //        var response = await httpClient.GetAsync(url);
-        //        return await response.Content.ReadAsStringAsync();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return null;
-        //    }
-        //}
-
     }
 }
