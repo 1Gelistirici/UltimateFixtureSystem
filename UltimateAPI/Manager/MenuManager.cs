24b8a7542f3dfc99e3696297fd8847504946a708
Onur <onurylmz00@gmail.com>
2022-03-23 23:52:38 +0300

    Changed

---
+++ UltimateAPI/Manager/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimateAPI.Manager
+{
+    public class MenuManager : BaseManager
+    {
+        private static readonly object Lock = new object();
+        private static volatile MenuManager _instance;
+        public static MenuManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new MenuManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+
+        }
+
+
+
+
+    }
+}
