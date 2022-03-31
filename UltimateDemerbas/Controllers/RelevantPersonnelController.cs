f8c5c99c9c84fcdff04e5360609a3de9bc5a58f9
Onur <onurylmz00@gmail.com>
2022-03-31 23:52:15 +0300

    Changed

--- UltimateDemerbas/Controllers/RelevantPersonnelController.cs
+++ UltimateDemerbas/Controllers/RelevantPersonnelController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using UltimateAPI.Entities;
+using UltimateDemerbas.Manager;
+using UltimateDemerbas.Models.Tool;
 
 namespace UltimateDemerbas.Controllers
 {
-    public class RelevantPersonnelController : Controller
+    [CheckAuthorize]
+    public class RelevantPersonnelController : BaseController
     {
+        protected override int PageNumber { get; set; } = 0;
+        private readonly IHttpClientFactory _httpClientFactory;
+        RelevantPersonnelManager reveland;
+        public RelevantPersonnelController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            reveland = new RelevantPersonnelManager(_httpClientFactory);
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult GetRelevantPersonnels()
+        {
+            var result = reveland.GetRelevantPersonnels(new ReferansParameter { CompanyId = WorkingCompany });
+            return Content(result.Result);
+        }
+
+        public IActionResult DeleteRelevantPersonnel([FromBody] ReferansParameter parameter)
+        {
+            var result = reveland.DeleteRelevantPersonnel(parameter);
+            return Content(result.Result);
+        }
+
+        public IActionResult AddRelevantPersonnel([FromBody] RelevantPersonnel parameter)
+        {
+            parameter.CompanyRefId = WorkingCompany;
+            var result = reveland.AddRelevantPersonnel(parameter);
+            return Content(result.Result);
         }
     }
 }
--- UltimateDemerbas/Manager/RelevantPersonnelManager.cs
+++ UltimateDemerbas/Manager/RelevantPersonnelManager.cs
@@ -1,6 +1,29 @@
-﻿namespace UltimateDemerbas.Manager
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using UltimateAPI.Entities;
+
+namespace UltimateDemerbas.Manager
 {
-    public class RelevantPersonnelManager
+    public class RelevantPersonnelManager : BaseManager
     {
+        public RelevantPersonnelManager(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+        }
+
+
+        public Task<string> GetRelevantPersonnels(ReferansParameter parameter)
+        {
+            return GetApiParameter<ReferansParameter>("RelevantPersonnel/GetRelevantPersonnels", parameter);
+        }
+
+        public Task<string> DeleteRelevantPersonnel(ReferansParameter parameter)
+        {
+            return GetApiParameter<ReferansParameter>("RelevantPersonnel/DeleteRelevantPersonnel", parameter);
+        }
+
+        public Task<string> AddRelevantPersonnel(RelevantPersonnel parameter)
+        {
+            return GetApiParameter<RelevantPersonnel>("RelevantPersonnel/AddRelevantPersonnel", parameter);
+        }
     }
 }
